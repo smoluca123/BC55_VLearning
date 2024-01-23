a9ca93c84aa5ba729c3c787e87c1714d415cec34
tay52636 <phuongtay52636@gmail.com>
2024-01-23 19:00:12 +0700

    push page Events success 95%

--- src/modules/layout/pages/MainLayout.jsx
+++ src/modules/layout/pages/MainLayout.jsx
@@ -5,8 +5,6 @@ import Loading from '../../../components/Loading/Loading';
 import { useDispatch, useSelector } from 'react-redux';
 import { setIsLoading } from '../../../components/Loading/slices/loadingSlice';
 import { Footer } from '../../../components/Footer/Footer';
-import CoursesStats from '../../../components/TayDev/Courses/CoursesStats';
-import Events from '../../events/pages/Events';
 
 export default function MainLayout() {
   const { isLoading } = useSelector((state) => state.loading);
@@ -25,8 +23,6 @@ export default function MainLayout() {
         <Outlet />
       </div>
       <Footer />
-      <CoursesStats />
-      <Events />
     </div>
   );
 }
