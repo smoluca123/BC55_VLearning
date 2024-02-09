ab32f2ce2babade8ced7e083141c97b9293dde00
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-02-09 15:33:54 +0700

    add scroll to top

--- src/modules/layout/pages/MainLayout.jsx
+++ src/modules/layout/pages/MainLayout.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setIsLoading } from '../../../components/Loading/slices/loadingSlice';
 import { Footer } from '../../../components/Footer/Footer';
 import { Toaster } from 'react-hot-toast';
+import ScrollTop from '../../../components/ScrollTop';
 
 export default function MainLayout() {
   const { isLoading } = useSelector((state) => state.loading);
@@ -24,6 +25,7 @@ export default function MainLayout() {
         <Outlet />
       </div>
       <Footer />
+      <ScrollTop />
       <Toaster position="top-right" />
     </div>
   );
