798321c9c3b52291ca917b749ff79414e181d82b
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-11-25 15:10:29 +0700

    Update token baseAPI.js

--- src/apis/baseAPI.js
+++ src/apis/baseAPI.js
@@ -4,7 +4,7 @@ const baseAPI = axios.create({
   baseURL: 'https://elearningnew.cybersoft.edu.vn/api/',
   headers: {
     TokenCybersoft:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NSIsIkhldEhhblN0cmluZyI6IjI0LzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNjUwODgwMDAwMCIsIm5iZiI6MTY4Nzg4NTIwMCwiZXhwIjoxNzE2NjU2NDAwfQ.HsoestvkIN5Kub3jnAr8UddrPugJcxCsAm4QfMi4ZbU',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NSIsIkhldEhhblN0cmluZyI6IjI0LzA1LzIyMjIiLCJIZXRIYW5UaW1lIjoiMTcxNjUwODgwMDAwMCIsIm5iZiI6MTY4Nzg4NTIwMCwiZXhwIjoxNzE2NjU2NDAwfQ._zGig3qRiiBsp3ISyCdJnpwe6erHcB-KUpka3g0VMPE',
   },
   params: {
     maNhom: 'GP01',
